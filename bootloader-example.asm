21fb2c0dac1fd4f6a68c4dcdc25229bf9045d5b2
dariakts <204338@supinfo.com>
2016-09-05 22:50:50 +0200

    added license disclaimer

--- bootloader-example.asm
+++ bootloader-example.asm
@@ -1,3 +1,25 @@
+; Minimal Linux Bootloader
+; ========================
+
+; @ author:	Sebastian Plotz
+; @ version:	1.0
+; @ date:	24.07.2012
+
+; Copyright (C) 2012 Sebastian Plotz
+
+; Minimal Linux Bootloader is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; Minimal Linux Bootloader is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with Minimal Linux Bootloader. If not, see <http://www.gnu.org/licenses/>.
+
 ; Bootloader
 ; Memory organisation (as described by The Linux/x86 boot protocol):
 ; 0x07c00 - 0x07dff Bootloader + partition table + MBR magic number
